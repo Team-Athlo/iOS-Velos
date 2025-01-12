780ec0c84a3721f12da493d40cbddd26dab62d48
Ellen J <jcrescent61@gmail.com>
2025-01-12 12:38:07 +0900

    First project setting

---
+++ iOS-Velos/iOS-Velos/iOS_VelosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_VelosApp.swift
+//  iOS-Velos
+//
+//  Created by Ellen J on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_VelosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
